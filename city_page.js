cc64a5b9bc8418a10b99e06b3108ed8be7f4c1ca
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-02-24 16:10:41 +0100

    first part, until the places- everything works

--- city_page.js
+++ city_page.js
@@ -11,9 +11,24 @@ fetch(url, options)
   .then((res) => res.json())
   .then((data) => viewDestinations(data));
 
-function viewDestionations(destination) {
+function viewDestinations(destination) {
   console.log(destination);
   document.querySelector(
     "h1"
   ).textContent = `${destination.city} | ${destination.country}`;
+  document.querySelector(
+    ".background"
+  ).src = `https://kea21s-6eb0.restdb.io/media/${destination.imgcitybigpc}`;
+  document.querySelector(".city_name").textContent = destination.city;
+  document.querySelector(".big_des").textContent = destination.citydescription;
+  document.querySelector(".time_zone").textContent = destination.time;
+  document.querySelector(".money").textContent = destination.currency;
+  document.querySelector(".water").textContent = destination.waterprice;
+  document.querySelector(".coffee").textContent = destination.coffeeprice;
+  document.querySelector(".beer").textContent = destination.beerprice;
+  document.querySelector(".lunch").textContent = destination.lunchprice;
+  document.querySelector(".see_map").href = destination.maplocation;
+  document.querySelector(
+    ".map"
+  ).src = `https://kea21s-6eb0.restdb.io/media/${destination.imgcountrymap}`;
 }
